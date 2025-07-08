aed104e66fa0a7dfd6b78180cf0b46a2543b90b4
Nick Pio <nckrpio@gmail.com>
2025-07-07 18:43:36 -0700

    Initial Commit

---
+++ linearem/linearemApp.swift
@@ -0,0 +1,17 @@
+//
+//  linearemApp.swift
+//  linearem
+//
+//  Created by Nick Pio on 2025-07-07.
+//
+
+import SwiftUI
+
+@main
+struct linearemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ linearemTests/linearemTests.swift
@@ -0,0 +1,17 @@
+//
+//  linearemTests.swift
+//  linearemTests
+//
+//  Created by Nick Pio on 2025-07-07.
+//
+
+import Testing
+@testable import linearem
+
+struct linearemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
